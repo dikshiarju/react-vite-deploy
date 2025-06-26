e423a038bf108ce405ff04ba5de93af952169bfb
dikshitha c <dikshitha.c@ellucian.com>
2025-06-26 21:32:12 +0530

    Fix case-sensitive import for Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
-import Footer from "./components/footer";   
+import Footer from "./components/Footer";   
 
 function App() {
 
